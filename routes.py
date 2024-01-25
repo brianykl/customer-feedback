ae2cb9f133f10f2a579b64074d96719e229e57c7
brian <brian.yikun.li@gmail.com>
2024-01-24 23:49:56 -0500

    backend successfully recieves feedback submission from front end, have made my first official entry to postgres

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import redirect, url_for, render_template, request, jsonify
+from db import add_record, get_records, update_record, delete_record, id_generator
 
 
 @app.route('/')
@@ -9,12 +10,12 @@ def index():
 @app.route('/api/feedback', methods=['POST'])
 def recieve_feedback():
     response = request.json
-    feedback = response.get('feedback')
-    # process feedback, save to database
-    print("hoorah")
+    feedback_id = id_generator()
+    feedback = {
+        'id': feedback_id, 
+        'email': response.get('email'), 
+        'category': response.get('category'), 
+        'feedback_text': response.get('feedback')}
+    add_record("feedback", feedback)
     return jsonify({'message': 'feedback recieved successfully'})
 
-# @app.route('/submit', methods = ['POST'])
-# def submit():
-
-#     return redirect(url_for('/'))
