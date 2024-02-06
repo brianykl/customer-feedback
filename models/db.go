e6ded6271d95d84cf85b38a36077bb4895163642
brian <brian.yikun.li@gmail.com>
2024-02-06 13:30:51 -0500

    working create and query functions for db ops

--- models/db.go
+++ models/db.go
@@ -15,6 +15,30 @@ func Connect() *gorm.DB {
 		log.Fatalf("failed to connect database: %v", err)
 	}
 
-	log.Println("Connected to database successfully")
+	log.Println("connected to database successfully")
 	return db
+}
+
+func Insert(feedback *Feedback) error {
+	db := Connect()
+	result := db.Create(feedback)
+	if result.Error != nil {
+		log.Println("failed to insert feedback %v", result.Error)
+		return result.Error
+	}
+
+	log.Println("feedback inserted successfully")
+	return nil
+}
+
+func Execute(query string, args ...interface{}) error {
+	db := Connect()
+
+	// Execute the SQL statement
+	result := db.Exec(query, args...)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
